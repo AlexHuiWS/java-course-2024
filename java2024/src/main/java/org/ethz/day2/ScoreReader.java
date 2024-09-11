27e7d1f91c0441d794ec2712e2cb26ffe42377a8
AlexHuiWS <alex.huiwaishing@gmail.com>
2024-09-11 16:07:34 +0200

    Update codes based on the solution

--- java2024/src/main/java/org/ethz/day2/ScoreReader.java
+++ java2024/src/main/java/org/ethz/day2/ScoreReader.java
@@ -5,20 +5,44 @@ import java.util.Scanner;
 public class ScoreReader {
     public static void main(String[] args) {
         // Declare variables
-        int maxNumber = 5;
-        int currentNumber = 0;
-        double score;
+        int maxCount = 100;
+        int count = 0;
 
         // Read inputs
         Scanner input = new Scanner(System.in);
         System.out.println("Input scores: ");
-        double[] scores = new double[maxNumber];
-        do {
-            score = input.nextDouble();
-            scores[currentNumber++] = score;
-        } while (currentNumber < maxNumber && score >= 0);
+        double number = input.nextDouble();
+
+        // Store inputs
+        double[] scores = new double[maxCount];
+        while (number >= 0 && count < maxCount) {
+            scores[count] = number;
+            count++;
+            number = input.nextDouble();
+        }
         input.close();
 
+        // Calculate average
+        double sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += scores[i];
+        }
+        double avg = sum / count;
+
+        // Compare individual score with average
+        double belowAvgCount = 0;
+        for (int i = 0; i < count; i++) {
+            if (scores[i] < avg) {
+                belowAvgCount += 1;
+            }
+        }
+
+        // Print outputs
+        System.out.println("Total number of scores: " + count);
+        System.out.println("Average of score: " + avg);
+        System.out.println("== Result ==");
+        System.out.println("Number of scores below average: " + belowAvgCount);
+        System.out.println("Number of scores above or equal to average: " + (count -belowAvgCount));
 
     }
 }
