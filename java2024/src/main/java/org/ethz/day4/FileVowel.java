a7567ab85d529919ddead3e282fb4d5bfa0569be
AlexHuiWS <alex.huiwaishing@gmail.com>
2024-09-13 16:00:18 +0200

    Create FileVowel.java

---
+++ java2024/src/main/java/org/ethz/day4/FileVowel.java
@@ -0,0 +1,8 @@
+package org.ethz.day4;
+import java.util.Scanner;
+
+public class FileVowel {
+    public static void main(String[] args) {
+        java.io.File file = new java.io.File ("babynamesranking2001.txt")
+    }
+}
